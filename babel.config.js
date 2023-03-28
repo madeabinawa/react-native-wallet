396d1e9a9273d75f791dcdb0d6fc7432f5137226
abinavva <madeabinawa@gmail.com>
2023-03-28 21:21:06 +0800

    chore: add new deps/package

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     'nativewind/babel',
+    ['react-native-reanimated/plugin', {relativeSourceLocation: true}],
     [
       'module-resolver',
       {
