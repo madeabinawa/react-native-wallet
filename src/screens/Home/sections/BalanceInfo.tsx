068dfc4af31fc03eb032e8adaab28f9d1e7f03df
abinavva <madeabinawa@gmail.com>
2023-04-02 13:35:29 +0800

    fix(screen/home): default obscured card number and sections separation enhancement

--- src/screens/Home/sections/BalanceInfo.tsx
+++ src/screens/Home/sections/BalanceInfo.tsx
@@ -3,16 +3,16 @@ import {View, Text, TouchableOpacity} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import {toDollarCurrency} from '@utilities';
-
-export interface BalanceInfoProps {}
+import {useAuth} from '@hooks';
 
 export function BalanceInfo() {
+  const {balance} = useAuth();
   return (
     <View className="flex flex-col mt-8 px-3">
       <Text className="text-base text-slate-400 mb-2">Total Balance</Text>
       <View className="flex flex-row justify-between items-center">
         <Text className="text-4xl text-slate-700 dark:text-white ">
-          {toDollarCurrency(548460)}
+          {toDollarCurrency(balance)}
         </Text>
 
         <TouchableOpacity className="bg-slate-200 dark:bg-[#271a3c] rounded-2xl p-2 flex flex-row justify-between items-center">
--- src/screens/Home/sections/index.ts
+++ src/screens/Home/sections/index.ts
@@ -2,3 +2,4 @@ export * from './UserGreeting';
 export * from './BalanceInfo';
 export * from './RecentTransactions';
 export * from './TransactionCard';
+export * from './ModalNotifications';
